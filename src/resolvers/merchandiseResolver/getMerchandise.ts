b4d97c1e0582b0d8ac083fbc08d03e253a055e18
ThanhVinh <vinh@docosan.com>
2022-08-09 19:03:41 +0700

    sort by name

--- src/resolvers/merchandiseResolver/getMerchandise.ts
+++ src/resolvers/merchandiseResolver/getMerchandise.ts
@@ -1,10 +1,22 @@
-import { MerchandiseModel } from '@models';
+import { IMerchandise, MerchandiseModel } from '@models';
 import { Error } from '@config';
 import { Context } from '@types';
 
+const compare = (a: IMerchandise, b: IMerchandise) => {
+  if (a.merchandiseName < b.merchandiseName) {
+    return -1;
+  }
+  if (a.merchandiseName > b.merchandiseName) {
+    return 1;
+  }
+  return 0;
+};
+
 export const getMerchandise = async (_: any, _params: undefined, context: Context) => {
   if (!context.userId) throw new Error('unauthorized', '401');
-  return MerchandiseModel.find({
+
+  const merchandise: IMerchandise[] = await MerchandiseModel.find({
     stallCode: context.stallCode,
   });
+  return merchandise.sort(compare);
 };
